6c43ea3c9367004d7ce035cc092adeb4bffa5675
Angelo Di Massimo <Ange47RM@hotmail.com>
2023-11-13 21:32:50 +0000

    Added Artists and Songs controller

---
+++ Controllers/ArtistsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AngelosAPI.Dtos;
+
+namespace AngelosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtistsController : ControllerBase
+    {
+        private readonly MusicDbContext _dbContext;
+
+        public ArtistsController(MusicDbContext musicDbContext)
+        {
+            _dbContext = musicDbContext;
+        }
+
+        // GET: api/Artists
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ArtistDto>>> GetArtists()
+        {
+          if (_dbContext.Artists == null)
+          {
+              return NotFound();
+          }
+            return await _dbContext.Artists.ToListAsync();
+        }
+
+        // GET: api/Artists/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ArtistDto>> GetArtist(int id)
+        {
+          if (_dbContext.Artists == null)
+          {
+              return NotFound();
+          }
+            var artist = await _dbContext.Artists.FindAsync(id);
+
+            if (artist == null)
+            {
+                return NotFound();
+            }
+
+            return artist;
+        }
+
+        // PUT: api/Artists/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutArtist(int id, ArtistDto artist)
+        {
+            if (id != artist.Id)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Entry(artist).State = EntityState.Modified;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArtistExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Artists
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ArtistDto>> PostArtist(ArtistDto artist)
+        {
+          if (_dbContext.Artists == null)
+          {
+              return Problem("Entity set 'MusicDbContext.Artists'  is null.");
+          }
+            _dbContext.Artists.Add(artist);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction("GetArtistDto", new { id = artist.Id }, artist);
+        }
+
+        // DELETE: api/Artists/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteArtist(int id)
+        {
+            if (_dbContext.Artists == null)
+            {
+                return NotFound();
+            }
+            var artistDto = await _dbContext.Artists.FindAsync(id);
+            if (artistDto == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Artists.Remove(artistDto);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ArtistExists(int id)
+        {
+            return (_dbContext.Artists?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/SongsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AngelosAPI.Dtos;
+
+namespace AngelosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private readonly MusicDbContext _dbContext;
+
+        public SongsController(MusicDbContext musicDbContext)
+        {
+            _dbContext = musicDbContext;
+        }
+
+        // GET: api/Songs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SongDto>>> GetSongs()
+        {
+          if (_dbContext.Songs == null)
+          {
+              return NotFound();
+          }
+            return await _dbContext.Songs.ToListAsync();
+        }
+
+        // GET: api/Songs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SongDto>> GetSong(int id)
+        {
+          if (_dbContext.Songs == null)
+          {
+              return NotFound();
+          }
+            var song = await _dbContext.Songs.FindAsync(id);
+
+            if (song == null)
+            {
+                return NotFound();
+            }
+
+            return song;
+        }
+
+        // PUT: api/Songs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSong(int id, SongDto song)
+        {
+            if (id != song.Id)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Entry(song).State = EntityState.Modified;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SongExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Songs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<SongDto>> PostSong(SongDto song)
+        {
+          if (_dbContext.Songs == null)
+          {
+              return Problem("Entity set 'MusicDbContext.Songs'  is null.");
+          }
+            _dbContext.Songs.Add(song);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction("GetSongDto", new { id = song.Id }, song);
+        }
+
+        // DELETE: api/Songs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSong(int id)
+        {
+            if (_dbContext.Songs == null)
+            {
+                return NotFound();
+            }
+            var song = await _dbContext.Songs.FindAsync(id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Songs.Remove(song);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SongExists(int id)
+        {
+            return (_dbContext.Songs?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
